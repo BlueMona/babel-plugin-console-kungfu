b3c009cc8f6296de14ebf4373bf46cae3040edd5
Anri Asaturov <anri82@gmail.com>
2017-05-24 21:17:42 +0300

    shorten icebear path

--- index.js
+++ index.js
@@ -5,19 +5,26 @@ function isLogger(path, loggers) {
     });
 }
 visited = {}
+
+const skipString = 'node_modules/peerio-icebear/';
+
 function getVisitor(t) {
     return {
         CallExpression(path, options) {
             const loggers = options.opts.loggers || [{ pattern: 'console' }];
             if (!isLogger(path, loggers)) return;
 
-            const filePath = this.file.log.filename;
+            let filePath = this.file.log.filename;
 
             if (visited[filePath] && visited[filePath][path.node.start] === path.node.end) {
                 return;
             }
             visited[filePath] = visited[filePath] || {};
             visited[filePath][path.node.start] = path.node.end;
+
+            if (filePath.startsWith(skipString)) {
+                filePath = 'icebear/' + filePath.substring(skipString.length);
+            }
             const line = path.node.loc.start.line;
             const description = `${filePath}:${line}:${path.node.callee.property.name.toUpperCase()}`;
 
